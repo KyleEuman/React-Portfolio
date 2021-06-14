f6d1b3c770d965efe6f22ef4bbff41fa9b0e7f5a
Kyle Euman <euman_kyle@yahoo.com>
2021-06-14 17:23:45 -0400

    finished home page styling

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,12 @@
 import  React from 'react';
-import image from '../Assets/images/brimage.jpg'
+import image from '../Assets/images/bluebrimage.jpg'
 
 export default function Home() {
     return (
         <main>
-            <img src={image} alt="Galaxy Backround" className='absolute object-cover w-full h-full'/>
+            <img src={image} alt="blue backround" className='absolute object-cover w-full h-full'/>
             <section className='relative flex justify center min-h-screen pt-12 lg:pt-64 px-8'>
-        <h1>Hello I am Kyle Euman</h1>
+        <h1 className='text-6xl text-white font-bold leading-none lg: leading-snug '>Hello I am Kyle Euman</h1>
         </section>
         </main>
     )
--- src/Pages/NavBar.js
+++ src/Pages/NavBar.js
@@ -1,25 +1,30 @@
 import  React from 'react';
 import {NavLink} from 'react-router-dom';
+import {SocialIcon } from 'react-social-icons';
 
 export default function NavBar() {
     return (
-        <header>
+        <header className="bg-blue-600">
             <div className="container mx-auto flex justify-between">
                 <nav className='flex'>
-                    <NavLink to='/' exact className='inflex-flex items-center py-6 px-3 mr-4'>
+                    <NavLink to='/' exact className='inflex-flex items-center py-6 px-3 mr-4 text-white hover:text-black text-4xl font-bold tracking-widest'>
                         Home
                     </NavLink>
-                    <NavLink to='/post'className='inflex-flex items-center py-6 px-3 mr-4'>
+                    <NavLink to='/post'className='inflex-flex items-center py-6 px-3 mr-4 text-white hover:text-black text-4xl font-bold tracking-widest'>
                         Blog Posts
                     </NavLink>
-                    <NavLink to='/Projects'className='inflex-flex items-center py-6 px-3 mr-4'>
+                    <NavLink to='/Projects'className='inflex-flex items-center py-6 px-3 mr-4 text-white hover:text-black text-4xl font-bold tracking-widest'>
                         Projects
                     </NavLink>
-                    <NavLink to='/About'className='inflex-flex items-center py-6 px-3 mr-4'>
+                    <NavLink to='/About'className='inflex-flex items-center py-6 px-3 mr-4 text-white hover:text-black text-4xl font-bold tracking-widest'>
                         About Me!
                     </NavLink>
                     
                 </nav>
+                <div className='inline-flex py-3 px-3 my-6'>
+                    <SocialIcon url="https://www.linkedin.com/in/kyle-euman-b51859203/" classname='mr-4' target='_blank' fgcolor='#fff' style={{height: 35, width: 35}} />
+                <SocialIcon url="https://github.com/KyleEuman" classname='mr-4' target='_blank' fgcolor='#ffff' style={{height: 35, width: 35}} />
+                </div> 
             </div>
         </header>
     )
